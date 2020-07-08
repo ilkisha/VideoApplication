2d229a690a9108c457278583d5822f9fb105ea80
ilkisha <i.shalvarkov@gmail.com>
2020-07-08 10:14:19 +0300

    Added CategoryFixtures and migration

--- migrations/Version20200708070451.php
+++ migrations/Version20200708070451.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200707121300 extends AbstractMigration
+final class Version20200708070451 extends AbstractMigration
 {
     public function getDescription() : string
     {
--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -11,13 +11,30 @@ class CategoryFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $this->loadMainCategories($manager);
+        $this->loadSubcategories($manager, 'Electronics', 1);
     }
 
     private function loadMainCategories($manager)
     {
-        foreach ($this->getMainCategoriesData() as [$name]){
+        foreach($this->getMainCategoriesData() as [$name] )
+        {
             $category = new Category();
             $category->setName($name);
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+
+    private function loadSubcategories($manager, $category, $parent_id)
+    {
+        $parent = $manager->getRepository(Category::class)->find($parent_id);
+        $methodName = "get{$category}Data";
+        foreach($this->$methodName() as [$name] )
+        {
+            $category = new Category();
+            $category->setName($name);
+            $category->setParent($parent);
             $manager->persist($category);
         }
 
@@ -30,7 +47,16 @@ class CategoryFixtures extends Fixture
             ['Electronics', 1],
             ['Toys', 2],
             ['Books', 3],
-            ['Movies', 4]
+            ['Movies', 4],
+        ];
+    }
+
+    private function getElectronicsData()
+    {
+        return [
+            ['Cameras', 5],
+            ['Computers', 6],
+            ['Cell Phones', 7]
         ];
     }
 }
