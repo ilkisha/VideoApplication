14577e90b71910131311a97263a624c5a828a60a
ilkisha <i.shalvarkov@gmail.com>
2020-07-09 17:41:45 +0300

    Finished displaying categories on front-page

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -26,10 +26,11 @@ class FrontController extends AbstractController
      */
     public function videoList($id, CategoryTreeFrontPage $categories)
     {
-        $subcategories = $categories->buildTree($id);
+        $categories->getCategoryListAndParent($id);
+
 
         return $this->render('front/video_list.html.twig', [
-            'subcategories' => $categories->getCategoryList($subcategories)
+            'subcategories' => $categories
         ]);
     }
 
