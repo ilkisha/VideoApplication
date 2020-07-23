f92b09720a0fcf7de705b4d0cd44fe9d9baed50b
ilkisha <i.shalvarkov@gmail.com>
2020-07-23 16:57:09 +0300

    Maked UserFixtures

--- migrations/Version20200723135406.php
+++ migrations/Version20200723135406.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200723133726 extends AbstractMigration
+final class Version20200723135406 extends AbstractMigration
 {
     public function getDescription() : string
     {
